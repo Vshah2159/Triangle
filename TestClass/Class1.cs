0ca801c6b1477324624df603cb7983177963affa
Vshah2159 <Vshah2159@conestogac.on.ca>
2024-03-06 18:02:51 -0500

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace TestClass
+{
+    public class Class1
+    {
+        // Valid Equilateral Triangle Test
+        [Test]
+        public void AnalyzeTriangle_ValidEquilateralTriangle_ReturnsEquilateral()
+        {
+            // Arrange
+            int side1 = 5;
+            int side2 = 5;
+            int side3 = 5;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual("An equilateral triangle is formed", result);
+        }
+    }
+}
