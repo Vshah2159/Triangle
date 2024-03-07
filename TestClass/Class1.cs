4f5f3fc8866ce4c4d6855b284499c599e2e255ea
Vshah2159 <Vshah2159@conestogac.on.ca>
2024-03-06 21:44:29 -0500

    add changes in isosceles

--- TestClass/Class1.cs
+++ TestClass/Class1.cs
@@ -14,7 +14,7 @@ namespace TestClass
 
         // Valid Equilateral Triangle Test
         [Test]
-        public void AnalyzeTriangle_ValidEquilateralTriangle_ReturnsEquilateral()
+        public void AnalyzeTriangle_Input5and5and5_OutputValidEquilateralTriangle()
         {
             // Arrange
             int side1 = 5;
@@ -30,12 +30,12 @@ namespace TestClass
 
         // Valid Isosceles Triangle Tests
         [Test]
-        public void AnalyzeTriangle_ValidIsoscelesTriangle1_ReturnsIsosceles()
+        public void AnalyzeTriangle_Input5and7and7_OutputValidIsoscelesTriangle1()
         {
             // Arrange
             int side1 = 5;
-            int side2 = 5;
-            int side3 = 6;
+            int side2 = 7;
+            int side3 = 7;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
@@ -45,12 +45,12 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_ValidIsoscelesTriangle2_ReturnsIsosceles()
+        public void AnalyzeTriangle_Input180and100and180_OutputValidIsoscelesTriangle2()
         {
             // Arrange
-            int side1 = 5;
-            int side2 = 6;
-            int side3 = 5;
+            int side1 = 180;
+            int side2 = 100;
+            int side3 = 180;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
@@ -60,12 +60,12 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_ValidIsoscelesTriangle3_ReturnsIsosceles()
+        public void AnalyzeTriangle_Input180and100and180_OutputValidIsoscelesTriangle3()
         {
             // Arrange
-            int side1 = 6;
-            int side2 = 5;
-            int side3 = 5;
+            int side1 = 98;
+            int side2 = 65;
+            int side3 = 65;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
@@ -76,12 +76,12 @@ namespace TestClass
 
         // Valid Scalene Triangle Tests
         [Test]
-        public void AnalyzeTriangle_ValidScaleneTriangle1_ReturnsScalene()
+        public void AnalyzeTriangle_Input20and7and15_OutputValidScaleneTriangle1()
         {
             // Arrange
-            int side1 = 3;
-            int side2 = 4;
-            int side3 = 5;
+            int side1 = 20;
+            int side2 = 7;
+            int side3 = 15;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
@@ -91,12 +91,12 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_ValidScaleneTriangle2_ReturnsScalene()
+        public void AnalyzeTriangle_Input50and80and120_OutputValidScaleneTriangle2()
         {
             // Arrange
-            int side1 = 5;
-            int side2 = 6;
-            int side3 = 7;
+            int side1 = 50;
+            int side2 = 80;
+            int side3 = 120;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
@@ -106,7 +106,7 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_ValidScaleneTriangle3_ReturnsScalene()
+        public void AnalyzeTriangle_Input8and10and12_OutputValidScaleneTriangle3()
         {
             // Arrange
             int side1 = 8;
@@ -121,7 +121,7 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_ValidScaleneTriangle4_ReturnsScalene()
+        public void AnalyzeTriangle_Input5and12and13_OutputValidScaleneTriangle4()
         {
             // Arrange
             int side1 = 5;
@@ -136,7 +136,7 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_ValidScaleneTriangle5_ReturnsScalene()
+        public void AnalyzeTriangle_Input7and15and20_OutputValidScaleneTriangle5()
         {
             // Arrange
             int side1 = 7;
@@ -152,12 +152,12 @@ namespace TestClass
 
         // Zero Length For One or More Side Tests
         [Test]
-        public void AnalyzeTriangle_ZeroLengthSide_ReturnsInvalid()
+        public void AnalyzeTriangle_Input0and55and56_OutputZeroLengthSide1()
         {
             // Arrange
             int side1 = 0;
-            int side2 = 4;
-            int side3 = 5;
+            int side2 = 55;
+            int side3 = 56;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
@@ -167,12 +167,12 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_ZeroLengthSide2_ReturnsInvalid()
+        public void AnalyzeTriangle_Input130and0and64_OutputZeroLengthSide2()
         {
             // Arrange
-            int side1 = 3;
+            int side1 = 130;
             int side2 = 0;
-            int side3 = 6;
+            int side3 = 64;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
@@ -182,11 +182,11 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_ZeroLengthSide3_ReturnsInvalid()
+        public void AnalyzeTriangle_Input45and80and0_OutputZeroLengthSide3()
         {
             // Arrange
-            int side1 = 4;
-            int side2 = 5;
+            int side1 = 45;
+            int side2 = 80;
             int side3 = 0;
 
             // Act
@@ -198,7 +198,7 @@ namespace TestClass
 
         // Invalid Response Tests
         [Test]
-        public void AnalyzeTriangle_InvalidResponse1_ReturnsInvalid()
+        public void AnalyzeTriangle_Input1and1and10_OutputInvalidResponse1()
         {
             // Arrange
             int side1 = 1;
@@ -213,12 +213,12 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_InvalidResponse2_ReturnsInvalid()
+        public void AnalyzeTriangle_Input24and35and111_OutputInvalidResponse2()
         {
             // Arrange
-            int side1 = 2;
-            int side2 = 3;
-            int side3 = 10;
+            int side1 = 24;
+            int side2 = 35;
+            int side3 = 111;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
@@ -228,12 +228,12 @@ namespace TestClass
         }
 
         [Test]
-        public void AnalyzeTriangle_InvalidResponse3_ReturnsInvalid()
+        public void AnalyzeTriangle_Input85and150and32_OutputInvalidResponse3()
         {
             // Arrange
-            int side1 = 7;
-            int side2 = 2;
-            int side3 = 5;
+            int side1 = 85;
+            int side2 = 150;
+            int side3 = 32;
 
             // Act
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
